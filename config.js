8b4b8affda39492c19cf149d2beca01e04147338
kavee_malli <tharakakavindu694@gmail.com>
2025-02-16 19:17:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //  සෙසන් අයිඩී එක දාන්න👇👇👇 (උඩු කොමා දෙක ඇතුලේ)
-  SESSION_ID: process.env.SESSION_ID || "", 
+  SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-o4oXkJJI#0n0MjV8dv2HxCzJc6OCZuanXQiEAVxKZ4XFJgl5yeYAftrc", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://tharakakavindu694:2ffyBwM5AbOClbAg@cluster0.9tgpt.mongodb.net/",
 
 
 MAX_SIZE: 500,
